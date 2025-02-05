ba76b5314e13689fe65482908a118ba40e444d9c
colin <tresscd@gmail.com>
2025-02-05 10:27:05 -0500

    Add server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
 import { TicTacToe } from "../games/tictactoe";
 import { TicTacToeBoard } from "./board";
 
-const App = Client({ game: TicTacToe, board: TicTacToeBoard });
+const server = "http://localhost:3000/";
+
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: server }),
+});
 
 export default App;
